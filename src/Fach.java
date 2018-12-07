165e27bf28d2e155ab4fdba2bf03b84080a48f88
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-07 15:21:42 +0100

    Created class Fach

---
+++ src/Fach.java
@@ -0,0 +1,16 @@
+
+public class Fach {
+	private String name;
+	private int wochenstunden;
+	private Raumtyp raumanforderungen;
+	
+	public String getName() {
+		return this.name;
+	}
+	public int getWochenstunden() {
+		return this.wochenstunden;
+	}
+	public Raumtyp getRaumanforderung() {
+		
+	}
+}
