8cc7e4b23a0a7d63bf9dcd3c20639710823a615b
Adrian Blanda <blanda.adrian@gmail.com>
2018-12-07 15:48:32 +0100

    Implement Person

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,38 @@
+import java.util.Date;
 
 public class Person {
+	private long svrn;
+	private String vorname;
+	private String nachname;
+	private Date geburtsdatum;
+	private String email;
+	
+	public Person(long svrn, String vorname, String nachname, Date geburtsdatum, String email) {
+		this.svrn = svrn;
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.geburtsdatum = geburtsdatum;
+		this.email = email;
+	}
+	
+	
+	public long getSvnr() {
+		return this.svrn;
+	}
 
+	public String getVorname() {
+		return this.vorname;
+	}
+
+	public String getNachname() {
+		return this.nachname;
+	}
+	
+	public Date getGeburtsdatum() {
+		return this.geburtsdatum;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
 }
