6c25a3d22d7f98a27eeb90e2b20f8f2c49f63316
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-07 15:32:15 +0100

    Created class Belegung

---
+++ src/Belegung.java
@@ -0,0 +1,23 @@
+
+public class Belegung {
+	
+	public enum Unterrichtstag{
+		MONTAG,
+		DIENSTAG,
+		MITTWOCH,
+		DONNERSTAG,
+		FREITAG;
+	}
+	
+	private int unterrichtsEinheit;
+	
+	public Unterrichtstag getWochentag() {
+		return 
+	}
+	public Klasse getKlasse() {
+		return
+	}
+	public Lehrer getLehrer() {
+		return
+	}
+}
