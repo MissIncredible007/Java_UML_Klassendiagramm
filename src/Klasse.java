489fc17746cbbd5600a177743d03f751702099a5
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-07 15:01:37 +0100

    Created class Klasse

---
+++ src/Klasse.java
@@ -0,0 +1,30 @@
+
+public class Klasse {
+
+	private String bezeichnung;
+	private int schulstufe;
+	
+	public String getBeizeichnung(){
+		return this.bezeichnung;
+	}
+	public int getSchulstufe() {
+		return this.schulstufe;
+	}
+	public float getDurchschnittsalter() {
+			
+		return durchschnittsalter;
+	}
+	public boolean setKlassensprecher(Schueler schueler) {
+		return 
+	}
+	public boolean addSchueler(Schueler schueler){
+		return		
+				
+	}
+	public void exportStundenplan() {
+		
+		
+	}
+
+
+}
