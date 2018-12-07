207685abfa7106c776df22ac8f0e171f1a1aa1f0
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-07 14:40:50 +0100

    Created class Schueler

---
+++ src/Schueler.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class Schueler extends Person {
+	
+	Schueler(int katalognummer, boolean eigenberechtigung, Date eintrifftsdatum) {
+	}
+	
+	
+	
+	
+	private int katalognummer;
+	private boolean eigenberechtigung;
+	private Date eintrifttsdatum;
+	
+	public  int getKatalognummer() {
+		return this.katalognummer;
+	}
+	
+	public boolean isEigenberechtigt() {
+		return this.eigenberechtigung;
+	}
+	public Date getEintrittsdatum() {
+		
+	}
+}
