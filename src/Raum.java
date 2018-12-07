5ec3867a4379160438230ad78cfd93a0dc25426c
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-07 15:10:28 +0100

    Create class Raum

---
+++ src/Raum.java
@@ -0,0 +1,19 @@
+
+public class Raum {
+	private String raumNummer;
+	private int maxSitzplaetze;
+	private Raumtyp raumtyp;
+	
+	public String getRaumNummer() {
+		return this.raumNummer;
+	}
+	public void exportBelegung() {
+		
+	}
+	public int getMaxSitzplaetze() {
+		return this.maxSitzplaetze;
+	}
+	public Raumtyp getRaumtype() {
+		return this.raumtyp;
+	}
+}
