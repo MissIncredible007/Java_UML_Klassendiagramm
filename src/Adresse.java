dd9a80e6afa6eae0483d17a2ded9a57015c768cf
Adrian Blanda <blanda.adrian@gmail.com>
2018-12-10 12:54:39 +0100

    Implemented Adresse

--- src/Adresse.java
+++ src/Adresse.java
@@ -1,4 +1,16 @@
 
 public class Adresse {
+	
+	private String ort;
+	private String strasse;
+	private int hausnummer;
+	private int plz;
 
+	Adresse(String ort, String strasse, int hausnummer, int plz){
+		this.ort = ort;
+		this.strasse = strasse;
+		this.hausnummer = hausnummer; 
+		this.plz = plz;
+		
+	}
 }
