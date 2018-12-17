5cb102e887ebce188c14cfc9da4645406a62db1c
Adrian Blanda <blanda.adrian@gmail.com>
2018-12-17 12:24:39 +0100

    Added Methods to Class Schule

--- src/Schule.java
+++ src/Schule.java
@@ -2,16 +2,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Schule {
+	private String name;
+	private Long schulkennzahl;
 	private List<Abteilung> abteilungen = new ArrayList<>();
 	
 	public List<Schueler> getSchueler() {
 		List<Schueler> alleSchueler = new ArrayList<>();
 		for (Abteilung a : abteilungen) {
-			for(Klasse k : a.getKlassen()) {
-				alleSchueler.add(k.getSchueler());	
-			}
+			alleSchueler.addAll(a.getSchueler());
 		}
 		return alleSchueler;
 		
 	}
+
+	public void addAbteilung(String name, String kuerzel) {
+		Abteilung neueAbteilung = new Abteilung(name, kuerzel);
+		this.abteilungen.add(neueAbteilung);
+		//return neueAbteilung;
+	}
+		
+	private class Abteilung {
+		private String kuerzel;
+		private String name;
+		private List<Klasse> klassen = new ArrayList<>();
+		
+		public Abteilung(String name, String kuerzel) {
+			this.name = name;
+			this.kuerzel = kuerzel;	
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public String getKuerzel() {
+			return this.kuerzel;
+		}
+		
+		public List<Schueler> getSchueler() {
+			List<Schueler> alleSchueler = new ArrayList<>();
+			for (Klasse k : klassen) {
+				alleSchueler.addAll(k.getSchueler());
+			}
+			return alleSchueler;
+		}
+
+		public List<Klasse> getKlassen() {
+			return klassen;
+		}
+	}
+
 }
