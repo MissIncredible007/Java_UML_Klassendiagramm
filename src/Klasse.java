189d88d150c714625472f55d4e93f8d70f35959d
MissIncredible007 <45660123+MissIncredible007@users.noreply.github.com>
2018-12-30 11:09:36 +0100

    Update Klasse.java

--- src/Klasse.java
+++ src/Klasse.java
@@ -8,22 +8,19 @@ public class Klasse {
 
 	private String bezeichnung;
 	private int schulstufe;
+	TreeSet<Schueler> schuelerListe = new TreeSet<>(new NachnameVergleich());
 	
 	Klasse(String bezeichnung, int schulstufe){
 		this.bezeichnung = bezeichnung;
 		this.schulstufe = schulstufe;
 	}
-	
-	
-	TreeSet<Schueler> schuelerListe = new TreeSet<Schueler>(new NachnameVergleich());
-	
+		
 	class NachnameVergleich implements Comparator<Schueler>{
 		 
-	    public int compare(Schueler e1, Schueler e2) {
-	        return e1.getNachname().compareTo(e2.getNachname());
+	    public int compare(Schueler s1, Schueler s2) {
+	        return s1.getNachname().compareTo(s2.getNachname());
 	    }
 	}
-	
 	
 	public String getBezeichnung(){
 		return this.bezeichnung;
@@ -31,62 +28,65 @@ public class Klasse {
 	public int getSchulstufe() {
 		return this.schulstufe;
 	}
-	
-	
-	
-	
+
 	public float getDurchschnittsalter() {
-		int summe = 0;
+		float summe = 0;
 		for(Schueler S : schuelerListe) {
 			summe += S.getAlter();
 		}
 		return summe / schuelerListe.size();
 	}
 	
-	
-	
 	public boolean addSchueler(Schueler schueler){
+		
 		if(this.schuelerListe.size() <= 36) {
 			this.schuelerListe.add(schueler);
+			// Klassenbezeichnung setzen
+			schueler.setKlassenbezeichnung(this.bezeichnung);
+			schueler.setSchulstufe(this.schulstufe);
+			// alle Katalognummern neu vergeben
+			int i = 0;
+			for(Schueler S : this.schuelerListe) {
+				i++;
+				S.setKatalognummer(i);
+			}
 			return true;
-		}
-		else {
-			return false;
-		}			
+			
+		} else return false;			
 	}
-	
 	
 	public boolean setKlassensprecher() {
 		// Ob es schon einen gibt
-		if(this.getKlassensprecher() != null) return false;
-		
-		int i=0, random = ThreadLocalRandom.current().nextInt(0, schuelerListe.size() + 1);
-		for(Schueler S : this.schuelerListe) {
-			if(i == random) {
-				S.setKlassensprecher(true);
-				return true;
+		try {
+			this.getKlassensprecher();
+		} catch (Exception e) { // Nur wenn es noch keinen Klassensprecher gibt, wählen
+			int i=0, random = ThreadLocalRandom.current().nextInt(0, schuelerListe.size());
+			//System.out.println("rand: "+random);
+			for(Schueler S : this.schuelerListe) {
+				//System.out.println("i: "+i);
+				if(i == random) {
+					S.setKlassensprecher(true);
+					return true;
+				}
+				i++;
 			}
-			i++;
 		}
 		return false;
 	}
 	
-	public Schueler getKlassensprecher() {
+	public Schueler getKlassensprecher() throws Exception {
 		for (Schueler S : this.schuelerListe) {
 			if(S.getKlassensprecher()) {
 				return S;
 			}
 		}
-		return null;
+		throw new Exception("Kein Klassensprecher gewählt!");
 	}
-	
 	
 	public void exportStundenplan() {
 		
-		
 	}
 	
-	//Verbindungen
 	public TreeSet<Schueler> getSchueler() {
 		return this.schuelerListe;
 	}
